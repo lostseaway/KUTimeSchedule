973ace6d586fa589a409a8ff2071bc01887a224c
LostSeaWay <lostseaway@me.com>
2014-11-24 15:14:24 +0700

    first commit

---
+++ src/testLoader.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
+
+public class testLoader {
+
+	/**
+	 * @param args
+	 * @throws IOException 
+	 */
+	public static void main(String[] args) throws IOException {
+		try{
+			String line;
+
+		    String cmd = "python ws.py 01219213 14 1";
+		    //System.out.println(cmd);
+		    Process p = Runtime.getRuntime().exec(cmd);
+		    BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream()));
+		    BufferedReader in =
+		            new BufferedReader(new InputStreamReader(p.getInputStream()));
+		    while ((line = in.readLine()) != null) {
+		        System.out.println(line); 
+		    }
+		    in.close();
+		} catch (Exception e) {}
+
+	}
+
+}
---
+++ ws.py
@@ -0,0 +1,42 @@
+import mechanize
+import sys
+from bs4 import BeautifulSoup
+
+# print 'Number of arguments:', len(sys.argv), 'arguments.'
+subject = sys.argv[1]
+year = sys.argv[2]
+sem = sys.argv[3]
+br = mechanize.Browser()
+br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
+req = br.open("https://inter-regis.ku.ac.th/_webcourse_data.php?key_field="+subject+"|"+year+"|"+sem)
+sub = br.response().read()
+soup = BeautifulSoup(sub)
+# print(soup.prettify())
+head = soup.p.string.encode("utf-8").replace("\xc2","")
+if "Course code not found" in head:
+	print head.split(":")[0]
+else:
+	head = head.split("\xa0")
+	while '' in head:
+		head.remove('')
+	print ("|").join(head)
+	sub = soup.findAll("tr")
+	out = []
+	for s in sub:
+		soup = BeautifulSoup(str(s))
+		stmp = soup.findAll("td")
+		line = []
+		tline = []
+		for x in stmp:
+			line.append(x.string)
+		for cts in line:
+			if (cts is None) or (len(cts) == 0) or cts == u'\xa0':
+				tline.append("NONE")
+				continue
+			tline.append(str((cts.replace(" ","").replace(u'\xa0',"")+" ")))
+		out.append(tline)
+	for x in out:
+		if len(x)==0:
+			continue
+		print ("|").join(x)
+
